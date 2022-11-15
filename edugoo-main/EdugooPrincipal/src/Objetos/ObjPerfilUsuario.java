24df65767c9aef703caca0152deab9a8e1d5e0ac
Ryan Souza22 <ryanp.souza@hotmail.com>
2022-11-15 04:39:38 -0300

    backend tela cadastro de perfil

---
+++ edugoo-main/EdugooPrincipal/src/Objetos/ObjPerfilUsuario.java
@@ -0,0 +1,78 @@
+package Objetos;
+
+public class ObjPerfilUsuario {
+    
+    private int perfilId;
+    private String perfilNome;
+    private String perfilDataNasc;
+    private String perfilSobreMim;
+    private String perfilInteresse1;
+    private String perfilInteresse2;
+    private String perfilStatusMercado;
+    private int perfilUsuarioId;
+
+    public int getPerfilId() {
+        return perfilId;
+    }
+
+    public void setPerfilId(int perfilId) {
+        this.perfilId = perfilId;
+    }
+
+    public String getPerfilNome() {
+        return perfilNome;
+    }
+
+    public void setPerfilNome(String perfilNome) {
+        this.perfilNome = perfilNome;
+    }
+
+    public String getPerfilDataNasc() {
+        return perfilDataNasc;
+    }
+
+    public void setPerfilDataNasc(String perfilData) {
+        this.perfilDataNasc = perfilData;
+    }
+
+    public String getPerfilSobreMim() {
+        return perfilSobreMim;
+    }
+
+    public void setPerfilSobreMim(String perfilSobreMim) {
+        this.perfilSobreMim = perfilSobreMim;
+    }
+
+    public String getPerfilInteresse1() {
+        return perfilInteresse1;
+    }
+
+    public void setPerfilInteresse1(String perfilInteresse1) {
+        this.perfilInteresse1 = perfilInteresse1;
+    }
+
+    public String getPerfilInteresse2() {
+        return perfilInteresse2;
+    }
+
+    public void setPerfilInteresse2(String perfilInteresse2) {
+        this.perfilInteresse2 = perfilInteresse2;
+    }
+
+    public String getPerfilStatusMercado() {
+        return perfilStatusMercado;
+    }
+
+    public void setPerfilStatusMercado(String perfilStatusMercado) {
+        this.perfilStatusMercado = perfilStatusMercado;
+    }
+
+    public int getPerfilUsuarioId() {
+        return perfilUsuarioId;
+    }
+
+    public void setPerfilUsuarioId(int perfilUsuarioId) {
+        this.perfilUsuarioId = perfilUsuarioId;
+    }
+
+}
